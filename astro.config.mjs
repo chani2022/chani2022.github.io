84e3252bb1363be5dfb0448b00fa9cec73e03842
Chani Andrianarinaivo <chaniandrianarinaivo@gmail.com>
2024-01-25 13:09:32 +0100

    -no-edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://chani2022.github.io',
-    base: '/chani2022.github.io',
 });
